7dff346806a0317aab8de0231055f23583b88c33
Zakaraie El Mejdki <elmejdki.zakariae@gmail.com>
2020-10-02 23:17:43 +0100

    Add a new font

--- webpack.config.js
+++ webpack.config.js
@@ -63,6 +63,12 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
+        ],
+      },
     ],
   },
   plugins: [
