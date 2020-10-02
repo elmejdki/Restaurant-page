a1af85a7525c9bef1f4f2a94d41b7439aa08ac32
Zakaraie El Mejdki <elmejdki.zakariae@gmail.com>
2020-10-02 22:22:50 +0100

    Add contact page

---
+++ src/pages/contact/contact.js
@@ -0,0 +1,99 @@
+import './contact.scss';
+import locationImg from "./images/location of zack's chicken.png";
+
+const contactInfos = [
+  {
+    title: 'Our address',
+    data: [
+      'Virmwska str., 174 Tetouan, Morocco',
+    ],
+  },
+  {
+    title: 'Work Schedule',
+    data: [
+      'Mon-Fri: 10:00-23:00',
+      'Sat-Sun: 10:00-00:00',
+    ],
+  },
+  {
+    title: 'Our Contacts',
+    data: [
+      '+212 675372051',
+      '+212 708170518',
+    ],
+  },
+];
+
+function render() {
+  const container = document.getElementById('content');
+  const pageTitle = document.createElement('div');
+  const title = document.createElement('h1');
+  const contactContainer = document.createElement('div');
+  const contactInfo = document.createElement('div');
+  const map = document.createElement('div');
+  const image = new Image();
+
+  let card;
+  let cardTitle;
+  let cardData;
+  let data;
+
+  contactInfos.forEach(info => {
+    card = document.createElement('div');
+    cardTitle = document.createElement('div');
+    cardData = document.createElement('div');
+
+    card.className = 'card';
+    cardTitle.className = 'title';
+    cardData.className = 'data';
+
+    cardTitle.textContent = info.title;
+
+    if (info.data.length > 1) {
+      info.data.forEach(d => {
+        data = document.createElement('span');
+
+        data.textContent = d;
+
+        cardData.appendChild(data);
+      });
+    } else {
+      /* eslint-disable array-bracket-spacing */
+      /* eslint-disable comma-dangle */
+      const [d, ] = info.data;
+      /* eslint-enable array-bracket-spacing */
+      /* eslint-enable comma-dangle */
+      cardData.textContent = d;
+    }
+
+    card.appendChild(cardTitle);
+    card.appendChild(cardData);
+    contactInfo.appendChild(card);
+  });
+
+  container.className = 'contact-page';
+  map.className = 'map';
+  image.src = locationImg;
+  pageTitle.className = 'page-title';
+  contactContainer.className = 'container';
+  contactInfo.className = 'contact-info';
+
+  container.innerHTML = '';
+  title.innerHTML = '<span>Our</span> Contacts';
+
+  pageTitle.appendChild(title);
+  container.appendChild(pageTitle);
+
+  map.appendChild(image);
+  contactContainer.appendChild(contactInfo);
+  contactContainer.appendChild(map);
+  container.appendChild(contactContainer);
+
+  const activeLink = document.querySelector('.active-link');
+  activeLink.className = '';
+
+  const contactBtn = document.getElementById('contact');
+  contactBtn.className = 'active-link';
+}
+
+export default render;
